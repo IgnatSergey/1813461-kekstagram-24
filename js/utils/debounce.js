d5eeeee1c5863f454155f44498c575f683a02d52
Keks <keks@htmlacademy.ru>
2021-11-17 13:10:15 +0300

    :package:

--- js/utils/debounce.js
+++ js/utils/debounce.js
@@ -1,23 +1,22 @@
-const debounce = (func, wait, immediate) => {
-  let timeout;
+// Функция взята из интернета и доработана
+// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
 
-  return function executedFunction() {
-    const context = this;
-    const args = arguments;
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
 
-    const later = function () {
-      timeout = null;
-      if (!immediate) { func.apply(context, args); }
-    };
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
 
-    const callNow = immediate && !timeout;
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
 
-    clearTimeout(timeout);
-
-    timeout = setTimeout(later, wait);
-
-    if (callNow) { func.apply(context, args); }
+    // Таким образом цикл "поставить таймаут - удалить таймаут" будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
   };
-};
+}
 
-export { debounce };
+export {debounce};
